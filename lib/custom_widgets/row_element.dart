3366bddeb3eb4fa8cbe3b5eb46754fda94ca1052
Borislav <borislav.domazet@sipodsoftware.com>
2024-07-17 18:07:57 +0200

    animations fix

--- lib/custom_widgets/row_element.dart
+++ lib/custom_widgets/row_element.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class RowElement extends StatelessWidget {
+class RowElement extends StatefulWidget {
   const RowElement({
     required this.onTap,
     required this.image,
@@ -10,19 +10,57 @@ class RowElement extends StatelessWidget {
 
   final String image;
   final Function onTap;
+
+  @override
+  State<RowElement> createState() => _RowElementState();
+}
+
+class _RowElementState extends State<RowElement> with TickerProviderStateMixin {
+  late final AnimationController imageAnimationController = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 130),
+  );
+
+  late final Animatable<double> imageRotationTween = Tween<double>(
+    begin: 0,
+    end: 300,
+  ).chain(
+    CurveTween(curve: Curves.bounceInOut),
+  );
+
+  double get imageRotation =>
+      imageRotationTween.evaluate(imageAnimationController);
+
+  @override
+  void initState() {
+    super.initState();
+    imageAnimationController.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    imageAnimationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: GestureDetector(
-        onTap: () {
+        onTap: () async {
           HapticFeedback.lightImpact();
-          onTap();
+          widget.onTap();
+          await imageAnimationController.forward();
+          await imageAnimationController.reverse();
         },
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Image.asset(
-            image,
-            fit: BoxFit.contain,
+          child: Transform.rotate(
+            angle: imageRotation,
+            child: Image.asset(
+              widget.image,
+              fit: BoxFit.contain,
+            ),
           ),
         ),
       ),
